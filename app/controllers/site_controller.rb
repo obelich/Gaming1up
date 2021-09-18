f0fb1445ce3049a9b121d90e9f44da63d3ede35e
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-09-18 12:11:25 -0700

    iniciando layout para listado de productos para el cliente

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,12 @@
 class SiteController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def contact
+  end
+
+  def product
+    @product = Product.find_by(id:params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  root 'site#index'
+
+
   resources :addresses
   devise_for :users
   resources :products
   resources :categories
-  root 'site#index'
+  get 'site/index'
   get 'site/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'site/product/:id', to: 'site#product', as: 'product_detail'
 end
