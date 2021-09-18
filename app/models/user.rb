c4cfc6bae8f06d7d66654caef847bf02cf88c48a
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-09-17 17:34:32 -0700

    agregando algunas configuraciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :timeoutable
 
   after_create :assign_default_role
 
   def assign_default_role
-    self.add_role(:newuser) if self.roles.blank?
+    self.add_role(:client) if self.roles.blank?
   end
 end
