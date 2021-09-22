01c75b8177e1599159115ece45a929ba3dc420ba
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-09-21 22:01:01 -0700

    se dio mas estilo a la parte de mostrar los productos

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Gaming1up
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    I18n.default_locale = :es
   end
 end
