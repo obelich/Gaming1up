827b84ba060c95fbcb4ac3dc247417368405c884
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-09-25 13:20:01 -0700

    Continue trying to fix heroku deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
-import 'css/styles'
 import "@fortawesome/fontawesome-free/css/all"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Gaming1up
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     I18n.default_locale = :es
   end
 end
